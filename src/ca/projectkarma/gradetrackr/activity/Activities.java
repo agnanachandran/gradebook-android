04ab6a0ec7762fb7770acd3417c708301790373b
Anojh Gnanachandran <anojhgnanachandran@gmail.com>
2013-09-22 22:36:47 -0400

    Added TODO

--- src/ca/projectkarma/gradetrackr/activity/Activities.java
+++ src/ca/projectkarma/gradetrackr/activity/Activities.java
@@ -1,6 +1,7 @@
 package ca.projectkarma.gradetrackr.activity;
 
 public abstract class Activities {
+	// TODO: Add the other activities, and use these String constants when starting intents
 	public static final String TOP_LEVEL = "ca.projectkarma.gradetrackr.activity";
 	public static final String ADD_CATEGORY = TOP_LEVEL + "AddCategory";
 }
